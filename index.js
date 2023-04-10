b737a4d23f63b354335bf9e67d1c4166d5c3ea8f
rubywu0604 <lksh20602@gmail.com>
2023-04-10 18:56:22 +0800

    move html file to firstpage

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ app.use(express.static('/views'));
 app.use(express.static('/public'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/html', express.static(__dirname + '/public/html'));
 app.use(express.json({limit: '1mb'}));
 app.set("view engine", "ejs")
 //history list
